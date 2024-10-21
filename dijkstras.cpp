63757840ae9c756dbdef735c89065b7a55231b0b
OwenYoungblood <oyoungbl@vols.utk.edu>
2024-10-21 10:54:09 -0400

    Update dijkstras.cpp

--- dijkstras.cpp
+++ dijkstras.cpp
@@ -3,7 +3,8 @@ using namespace std;
 #include<iostream>
 #include<vector>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
+{
     int tileN;
     char tileName;
     int tileCost;
@@ -14,12 +15,14 @@ int main(int argc, char *argv[]) {
     int startCol;
     int endRow;
     int endCol;
+    int tmp;
     vector<char> tileNames;
     vector<char> tileCosts;
     vector<vector<int>> map;
 
     cin >> tileN;
-    for(int i = 0; i < tileN; i++){
+    for(int i = 0; i < tileN; i++)
+    {
         cin >> tileName;
         cin >> tileCost;
         tileNames.push_back(tileName);
@@ -29,12 +32,23 @@ int main(int argc, char *argv[]) {
     cin >> mapCols;
 
     map.resize(mapRows);
-    for(int i = 0; i < mapRows; i++){
+    for(int i = 0; i < mapRows; i++)
+    {
         map[i].resize(mapCols);
-        for(int j = 0; j < mapCols; j++){
+        for(int j = 0; j < mapCols; j++)
+        {
             cin >> tile;
-            map[i][j] = tile;
+            for(int k = 0; k < tileNames.size(); ++k)
+            {
+                if(tileNames[k] == tile)
+                {
+                    tmp = tileCosts[k];
+                }
+                map[i][j] = tile;
+            }
+            cout << map[i][j] << " ";
         }
+        cout << endl;
     }
 
     cin >> startRow;
